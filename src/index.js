642f2956a47aa8764ba884af554b9c174e04518c
flaviuever <flaviuever@gmail.com>
2022-03-28 15:58:55 +0300

    work in progress for categoryFilter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { render } from "react-dom";
-
 import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from "./App.jsx";
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-
-
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}>
+    <App />
+</Auth0Provider>, document.getElementById("root")
+);
